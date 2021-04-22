3e026a579b6cba99a7b51010d173f2ce5f9442e3
mallikarjuna-kode <kanapuram97@gmail.com>
2021-04-22 11:39:40 +0530

    changes to connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-    $link = mysqli_connect("shareddb-o.hosting.stackcp.net", "secretdi-31303909eb", "(N)(M)(N)%", "secretdi-31303909eb");
+    $link = mysqli_connect("shareddb-o.hosting.stackcp.net", "secretdi-31303909eb", "Password", "secretdi-31303909eb");
         
         if (mysqli_connect_error()) {
             
